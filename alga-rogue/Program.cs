90e11a3a53b42c3a9b2e9ec11d58656a64ebf36d
Tom van Nimwegen <tomvannimwegen@gmail.com>
2017-10-09 10:51:13 +0200

    Add project files.

---
+++ alga-rogue/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alga_rogue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
